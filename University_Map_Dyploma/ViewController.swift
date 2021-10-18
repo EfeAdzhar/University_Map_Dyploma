57a5d89b9bf1ddeb15cc569e3d3df65dcf81dcd8
EfeAdzhar <efeadzhar@gmail.com>
2021-10-18 20:10:40 +0300

    Testing...

--- University_Map_Dyploma/ViewController.swift
+++ University_Map_Dyploma/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     @IBOutlet var arView: ARView!
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
+        setupARView()
+    }
+    // MARK: Setup methods
+    func setupARView() {
+        //Not working. Needs a physical device instead of a simulator.
+        //arView.automaticallyConfigureSession = false
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        configuration.environmentTexturing = .automatic
+        arView.session.run(configuration)
+        
     }
     
-    
-  }
+}
 
