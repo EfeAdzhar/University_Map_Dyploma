fae4bd8ea2b9be16c644691c508054895b726a88
EfeAdzhar <efeadzhar@gmail.com>
2021-10-18 16:42:29 +0300

    Initial Commit

---
+++ University_Map_Dyploma/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  University_Map_Dyploma
+//
+//  Created by Efe on 18.10.2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
