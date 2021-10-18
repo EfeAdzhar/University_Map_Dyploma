7c635b58faffc2af33b3d143ddb1d043d77219fa
EfeAdzhar <efeadzhar@gmail.com>
2021-10-18 16:55:03 +0300

    Starting

--- University_Map_Dyploma/ViewController.swift
+++ University_Map_Dyploma/ViewController.swift
@@ -7,18 +7,14 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet var arView: ARView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
-}
+    
+    
+  }
+
